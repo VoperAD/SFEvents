1e4e3e188e629090ee20810e760a08fd75caa51c
Conventional Changelog Action <conventional.changelog.action@github.com>
2025-08-03 02:09:53 +0000

    chore(release): version v0.2.1 [skip ci]

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "io.github.voperad"
-version = "0.2.0"
+version = "0.2.1"
 
 repositories {
     mavenCentral()
@@ -52,7 +52,7 @@ java {
 
 bukkit {
     name = "SFEvents"
-    version = "0.2.0"
+    version = "0.2.1"
     author = "VoperAD"
     main = "io.github.voperad.sfevents.SFEvents"
     apiVersion = "1.13"
